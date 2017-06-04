7898c13810423c4b60a11555f550628f562a38b6
Ksoy <ksoyam95@gmail.com>
2017-06-05 03:10:57 +0800

    change ip to domain name

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    $.get('http://140.113.215.24/reports/v1/api/record/'+request.user+'/'+request.report_id+'/'+request.cheater);
+    $.get('http://fuckagent.nctu.me/reports/v1/api/record/'+request.user+'/'+request.report_id+'/'+request.cheater);
 });
